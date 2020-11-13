b71c39daba84fd7b0afc2626aa63ef6d05a369d9
Keith Boynton <keithboynton@flaredesign.co.uk>
2020-11-13 05:53:51 +0000

    Added support for duplicating projects, getting a team by id and also very basic support for an asana job object

---
+++ AsanaNet/Objects/AsanaDuplicateProjectJob.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AsanaNet.Objects
+{
+    [Serializable]
+    public class AsanaDuplicateProjectJob : AsanaJob
+    {
+        [AsanaDataAttribute("new_project", SerializationFlags.Required)] //
+        public AsanaProject NewProject { get; set; }
+    }
+}
---
+++ AsanaNet/Objects/AsanaDuplicateProjectSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AsanaNet.Objects
+{
+    [Serializable]
+    public class AsanaDuplicateProjectSettings : AsanaObject
+    {
+        [AsanaDataAttribute("name", SerializationFlags.Required)] //
+        public string Name { get; set; }
+
+        [AsanaDataAttribute("include", SerializationFlags.Optional)] //
+        public string Include { get; set; }
+
+        [AsanaDataAttribute("schedule_dates", SerializationFlags.Optional)] //
+        public AsanaScheduleDates ScheduleDates { get; set; }
+    }
+}
